941bdd833ac941019596e8ddf252364600a2a3d2
Anna Moskalchuk <anna@crystax.net>
2019-03-18 11:48:28 +0200

    feat: add scorer of likes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import 'bootstrap';
 
 console.log ('Hello!');
 
-$('.score').text('55');
-let tmp = $(".tmp").text();
-console.log ("tmp = " + tmp);
+
+$(document).ready(() => {
+  for (let j=0; j<=4; j++) {
+    let max = $('.score').eq(j).text();
+    console.log ("MAX = " + max);
+
+    for (let i=1; i<=max; i++) {
+      setTimeout(() => {
+        $('.score').eq(j).text(i);
+      }, i*10 );
+    };
+  };
+});
+
